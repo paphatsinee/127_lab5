e181576182cefb830a697f213c41c801f406f16c
Paphatsinee <paphatsinee.r@ku.th>
2021-03-08 19:54:12 +0700

    Update Lab5_5.c

--- Lab5_5.c
+++ Lab5_5.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int N,M;
+    int N,M,i,j;
     scanf("%d",&N);
     scanf("%d",&M);
     int A[N][N];
     int B[M][M];
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            scanf("%d",&A[i][j]);
+        }
+    }
+
+    return 0;
 }
